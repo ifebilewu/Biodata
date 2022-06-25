dde16f4f2a2f549fc1c21082bc21aed6e62482b9
ifebilewu <ifeoluwabilewu10@gmail.com>
2022-06-25 13:52:16 +0100

    First commit

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-window.alert("My name is Bilewu Ifeoluwa");
-window.alert("I'm 1.68m tall");
-window.alert("I'm a Nigerian");
+const name_cont = document.createElement("h1");
+const name_data = document.createTextNode("Name: Bilewu Ifeoluwa");
+name_cont.appendChild(name_data);
+
+const name_display = document.getElementById("ife");
+name_display.appendChild(name_cont);
+
+
+const height_cont = document.createElement("h2");
+const height_data = document.createTextNode("Height: 5'6");
+height_cont.appendChild(height_data);
+
+const height_display = document.getElementById("ife");
+height_display.appendChild(height_cont);
+
+const country_cont = document.createElement("h2");
+const country_data = document.createTextNode("Country: Nigeria");
+country_cont.appendChild(country_data);
+
+const country_display = document.getElementById("ife");
+country_display.appendChild(country_cont);
