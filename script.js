05aa35075e75516f20ce8eedfd52e469f942b462
ifebilewu <ifeoluwabilewu10@gmail.com>
2022-06-25 14:38:47 +0100

    first commit

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const name_cont = document.createElement("h1");
-const name_data = document.createTextNode("Name: Bilewu Ifeoluwa");
-name_cont.appendChild(name_data);
-
-const name_display = document.getElementById("ife");
-name_display.appendChild(name_cont);
-
-
-const height_cont = document.createElement("h2");
-const height_data = document.createTextNode("Height: 5'6");
-height_cont.appendChild(height_data);
-
-const height_display = document.getElementById("ife");
-height_display.appendChild(height_cont);
-
-const country_cont = document.createElement("h2");
-const country_data = document.createTextNode("Country: Nigeria");
-country_cont.appendChild(country_data);
-
-const country_display = document.getElementById("ife");
-country_display.appendChild(country_cont);
+document.getElementById("name").innerHTML = "NAME: Bilewu Ifeoluwa";
+document.getElementById("height").innerHTML = "HEIGHT: 1.68m";
+document.getElementById("country").innerHTML = "COUNTRY: Nigeria";
